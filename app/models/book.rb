cb7254c156e0d7ef3c4eff5b753341ff4a677bec
benedetta castagna <benny.castagna@gmail.com>
2022-09-16 18:36:33 +0200

    seeded

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+  has_many :reviews
+
+  # callback on slugify: i want the title to be slugified before it's saved in the db
+  before_create :slugify
+
+  # slugify the title of the book so that is URL safe
+  def slugify
+    self.slug = title.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create([
+  {
+    title: "Divine Comedy",
+    image_url: "https://en.wikipedia.org/wiki/Divine_Comedy#/media/File:Dante_Domenico_di_Michelino_Duomo_Florence.jpg"
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    image_url: "https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg"
+  },
+  {
+    title: "The Chronicles of Narnia",
+    image_url: "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_box_set_cover.jpg"
+  },
+  {
+    title: "The Ultimate Hitchhiker's Guide to the Galaxy",
+    image_url: "https://images-na.ssl-images-amazon.com/images/I/A1lXgSfNdpL.jpg"
+  },
+  {
+    title: "The Great Gatsby",
+    image_url: "https://kbimages1-a.akamaihd.net/5addc4c9-fbc1-42d7-a79f-cec7619d4b23/1200/1200/False/the-great-gatsby-a-novel-1.jpg"
+  }
+
+])
+
+Review.create([
+  {title: 'Great!',
+  description: 'ahajjsksks',
+  score: 5,
+  book: Book.first},
+
+  {title: 'Did not like it!',
+    description: 'Very poorly written...',
+    score: 1,
+    book: Book.first}
+])
