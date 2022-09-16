5c0ed5137ef127b16c231c78f0ea6372bfc2d99d
benedetta castagna <benny.castagna@gmail.com>
2022-09-16 19:32:40 +0200

    set up the index as root, set up namespaces for the api and book and reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :books, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # route requests that are not predefined in the api back to the index path
+  # this because when we start using react router this would allow us to
+  # handle routing through our react component without actually interfereing with the rails routes for our api
+  get '*path', to: 'pages#index', via: :all
 end
